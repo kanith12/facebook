70756a982762ae41abc40b7a4f32ffc6e074aa28
kanith12 <indukurikanith@gmail.com>
2023-08-18 10:29:08 +0000

    update

--- sai.sh
+++ sai.sh
@@ -1,3 +1,9 @@
 this is edited in development branch 
 to cross check with diff bt both branche
+<<<<<<< HEAD
 xijojediiwnuihdjkfjiewbxs
+=======
+xijojediiwxgyugy
+hughjg
+s
+>>>>>>> fa09b9e (update)
